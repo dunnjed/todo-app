f0a478c874329d82cfa8574e75fad67d2a5ebe8a
JD <none>
2016-01-06 23:25:25 -0500

    Updated package.json.

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ var methodOverride = require('method-override'); // simulate DELETE and PUT (exp
 
 // configuration =================
 
-mongoose.connect('mongodb://localhost:27017/test');     // connect to mongoDB database on modulus.io
+mongoose.connect('mongodb://localhost:27017/test');     // connect to local mongoDB database 
 
 app.use(express.static(__dirname + '/public'));                 // set the static files location /public/img will be /img for users
 app.use(morgan('dev'));                                         // log every request to the console
